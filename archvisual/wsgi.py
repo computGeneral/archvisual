a43cf8a6dabe5896a6f22ac42af41e2f9e5ac4c5
computGene <computgene@gmail.com>
2026-07-17 11:35:16 +0800

    update copyright tips

--- archvisual/wsgi.py
+++ archvisual/wsgi.py
@@ -1,13 +1,13 @@
 """/usr/bin/env python3"""
 # The confidential and proprietary information contained in this file may 
 # only be used by a person authorized under and to the extent permitted   
-# by a subsisting licensing agreement from computGeneral Limited.                 
-#                 (c) Copyright 2024-2029 computGeneral Limited.                  
+# by a subsisting licensing agreement from lyj002286 limited.                 
+#                 (c) Copyright 2024-2029 lyj002286 limited.                  
 #                     ALL RIGHTS RESERVED                                 
 # This entire notice must be reproduced on all copies of this file        
 # and copies of this file may only be made by a person if such person is  
 # permitted to do so under the terms of a subsisting license agreement    
-# from computGeneral Limited.                                                     
+# from lyj002286 limited.                                                     
 # 
 # Filename        : ./visualizer/archvisual/wsgi.py
 # Last Revision   : 0.0.1
